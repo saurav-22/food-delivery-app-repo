47cfcfc62d549317f87ae99d483759cb169120b4
Saurav <pluginsforu@gmail.com>
2025-10-30 17:53:45 +0530

    Updated index.php in order-service

--- backend/order-service/public/index.php
+++ backend/order-service/public/index.php
@@ -4,7 +4,7 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$app = AppFactory::createFromContainer();;
 
 // Add body parsing and routing middleware
 $app->addBodyParsingMiddleware();
